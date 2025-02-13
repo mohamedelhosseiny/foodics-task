52cfc5722e3892a8c56b2dfeac04d6c7ba8c1b3e
hosseiny <mohamedelhosseiny10@gmail.com>
2025-02-13 09:29:00 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="min-h-screen bg-gray-100"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+@import "./assets/styles/main.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
