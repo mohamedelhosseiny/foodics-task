5298b0a65dc3ec288bf7aafe701c1e276ac01d6e
hosseiny <mohamedelhosseiny10@gmail.com>
2025-02-16 09:02:33 +0200

    refactor: extract reservations table into a separate component

--- src/components/branchesReservations.vue
+++ src/components/branchesReservations.vue
@@ -32,39 +32,7 @@
           </button>
         </div>
 
-        <div class="w-full">
-          <div class="flex border-b">
-            <div class="flex-1 text-left py-4 font-medium">Branch</div>
-            <div class="flex-1 text-left py-4 font-medium">Reference</div>
-            <div class="flex-1 text-left py-4 font-medium">
-              Number of Tables
-            </div>
-            <div class="flex-1 text-left py-4 font-medium">
-              Reservation Duration
-            </div>
-          </div>
-
-          <template v-if="areBranchesLoading">
-            <branch-reservation-item-skeleton v-for="i in 8" :key="i" />
-          </template>
-          <template v-else>
-            <template v-if="branchesAcceptingReservations.length > 0">
-              <branch-reservation-item
-                v-for="branch in branchesAcceptingReservations"
-                :key="branch.id"
-                :branch="branch"
-              />
-            </template>
-            <template v-else>
-              <div
-                class="text-center text-gray-500 py-4 min-h-[300px] flex items-center justify-center"
-              >
-                No branches accepting reservations, please enable reservations
-                for the branches you want to accept reservations for.
-              </div>
-            </template>
-          </template>
-        </div>
+        <reservations-table />
       </div>
     </div>
 
@@ -83,17 +51,15 @@ import {
   DISABLE_RESERVATIONS,
 } from "../store/modules/action-types";
 const { mapState, mapActions } = createNamespacedHelpers("branches");
-import BranchReservationItem from "./branchReservationItem.vue";
-import BranchReservationItemSkeleton from "./branchReservationItemSkeleton.vue";
+import ReservationsTable from "./reservationsTable.vue";
 import AddBranch from "./addBranch.vue";
 
 export default defineComponent({
   name: "BranchesReservations",
 
   components: {
-    BranchReservationItem,
-    BranchReservationItemSkeleton,
     AddBranch,
+    ReservationsTable,
   },
 
   data() {
