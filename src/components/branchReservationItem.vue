58284123dd72e2b51c4466650454de6727005315
hosseiny <mohamedelhosseiny10@gmail.com>
2025-02-18 15:04:59 +0200

    fix: show the tables accepting reservations count only

--- src/components/branchReservationItem.vue
+++ src/components/branchReservationItem.vue
@@ -2,7 +2,7 @@
   <div class="flex border-b hover:bg-gray-50 cursor-pointer">
     <div class="w-1/4 py-4">{{ branch.name }}</div>
     <div class="w-1/4 py-4">{{ branch.reference }}</div>
-    <div class="w-1/4 py-4">{{ tablesCount }}</div>
+    <div class="w-1/4 py-4">{{ tablesAcceptingReservations }}</div>
     <div class="w-1/4 py-4">{{ branch.reservation_duration }} Minutes</div>
   </div>
 </template>
@@ -17,11 +17,13 @@ export default {
   },
 
   computed: {
-    tablesCount() {
-      return this.branch.sections.reduce(
-        (acc, section) => acc + section.tables.length,
-        0
-      );
+    tablesAcceptingReservations() {
+      return this.branch.sections.reduce((acc, section) => {
+        return (
+          acc +
+          section.tables.filter((table) => table.accepts_reservations).length
+        );
+      }, 0);
     },
   },
 };
